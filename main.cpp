6a91015c410b4a2fc64d1055109f1840639071ea
Khoa Tran <nhatkhoa1124@gmail.com>
2026-04-18 03:02:30 +0700

    feat: Add SDL setup.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include <SDL3/SDL_main.h>
 #include "chip8.h"
 
-#define WINDOW_LENGTH = 640;
-#define WINDOW_HEIGHT = 320;
+#define WINDOW_WIDTH  640
+#define WINDOW_HEIGHT 320
 
 bool load_rom(chip8 &vm, const std::string &path) {
     std::ifstream file(path, std::ios::binary | std::ios::ate);
@@ -25,6 +25,22 @@ bool load_rom(chip8 &vm, const std::string &path) {
     }
     return true;
 }
+
+void setup() {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL Init failed: %s", SDL_GetError());
+        abort();
+    }
+    SDL_Window *window = SDL_CreateWindow("PicklEmul", WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        SDL_Log("Renderer setup failed: %s", SDL_GetError());
+        abort();
+    }
+    SDL_SetDefaultTextureScaleMode(renderer, SDL_SCALEMODE_NEAREST);
+    SDL_SetRenderLogicalPresentation(renderer, 64, 32, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+}
+
 
 int main() {
     return 0;
